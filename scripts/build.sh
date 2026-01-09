bd943aca15a134fa7caa617611f9a412c3bc1152
Elias Broschin <pixclstudio@gmail.com>
2026-01-09 10:10:25 -0800

    chore: add build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+./scripts/bootstrap_vcpkg.sh
+
+cmake -S . -B build -G Ninja \
+  -DCMAKE_TOOLCHAIN_FILE=external/vcpkg/scripts/buildsystems/vcpkg.cmake
+
+cmake --build build
