566567db14009f8275fcea95ce6cb59daa673646
Elias Broschin <pixclstudio@gmail.com>
2026-01-10 18:45:53 +0100

    chore: use bash for bootstrap script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-./scripts/bootstrap_vcpkg.sh
+bash ./scripts/bootstrap_vcpkg.sh
 
 cmake -S . -B build -G Ninja \
   -DCMAKE_TOOLCHAIN_FILE=external/vcpkg/scripts/buildsystems/vcpkg.cmake
