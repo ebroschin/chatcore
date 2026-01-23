e953a2d6529bc16b45832b786e37c5faf60c902d
Elias Broschin <pixclstudio@gmail.com>
2026-01-23 16:39:17 +0100

    chore: move files to network-modules library

--- source/application/chat_tcp_system.h
+++ source/application/chat_tcp_system.h
@@ -1,13 +1,12 @@
 #pragma once
 
-#include "../network_modules/connections/boost_sync/sync_boost_tcp_acceptor.h"
-#include "../network_modules/connections/boost_sync/sync_boost_tcp_resolver.h"
-#include "../network_modules/connections/boost_async/boost_tcp_acceptor.h"
+#include <claw/network-modules/connectors/boost_sync/sync_boost_tcp_resolver.h>
+#include <claw/network-modules/connectors/boost_async/boost_tcp_acceptor.h>
 
 #include <claw/network/tcp/tcp_system.h>
 #include <claw/network/tcp/tcp_system_builder.h>
+#include <claw/network-modules/codecs/json_network_codec.h>
 
-#include "../network_modules/codecs/json_network_codec.h"
 #include "messages/json_macros.h"
 
 namespace claw::network::modules {
