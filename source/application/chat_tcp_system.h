9ea1a8d8fd81d822ccf0beb617443a49d6d0b739
Elias Broschin <pixclstudio@gmail.com>
2026-01-29 18:08:09 +0100

    feat: add async resolver to test client

--- source/application/chat_tcp_system.h
+++ source/application/chat_tcp_system.h
@@ -1,20 +1,16 @@
 #pragma once
 
-#include <claw/network-modules/connectors/boost_sync/sync_boost_tcp_resolver.h>
+#include <claw/network-modules/connectors/boost_async/boost_tcp_resolver.h>
 #include <claw/network-modules/connectors/boost_async/boost_tcp_acceptor.h>
 #include <claw/network/tcp/tcp_system.h>
 #include <claw/network/tcp/tcp_system_builder.h>
 #include <claw/network-modules/codecs/json_network_codec.h>
 #include <claw/chat/json_api.h>
 
-namespace claw::network::modules {
-class BoostTcpAcceptor;
-}
-
 namespace claw::chat::server {
 
 using ChatClientTcpSystem = network::tcp::TcpSystemBuilder<
-    network::modules::SyncBoostTcpResolver,
+    network::modules::BoostTcpResolver,
     network::modules::JsonNetworkCodec,
     api::MessageTypes
 >::Type;
