7da916395e6a94bd2f4ac7a324140d614ae6aa6e
Elias <pixclstudio@gmail.com>
2026-04-16 17:53:57 +0200

    refactor(clients): move commands abstraction to commands library

--- apps/client/source/commands/client_commands_system.h
+++ apps/client/source/commands/client_commands_system.h
@@ -1,15 +1,15 @@
 #pragma once
 
+#include <ebroschin/commands/commands_system.hpp>
 #include "add_channel_command.h"
 #include "add_user_command.h"
-#include "commands_system.h"
 #include "connect_command.h"
 #include "exit_command.h"
 #include "get_channels_command.h"
+#include "help_command.h"
 #include "join_channel_command.h"
 #include "login_command.h"
 #include "logout_command.h"
-#include "help_command.h"
 
 namespace ebroschin::chatcore::client {
 
---
+++ libs/ebroschin-commands/tests/test_command_system.cpp
@@ -0,0 +1,11 @@
+#include <ebroschin/core/system.h>
+#include <ebroschin/core/system_context.h>
+#include <gtest/gtest.h>
+
+namespace ebroschin::commands::tests {
+
+TEST(CommandsSystemTests, TODO) {
+
+}
+
+}
